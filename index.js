fa0100b18c2505d2b63e12888bd11cff1029cfd6
jmike <josemimore0@gmail.com>
2024-05-07 14:20:55 -0400

    created church location file, and update cart view with modal

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const CART = document.getElementById("cart");
+const MODAL = document.getElementById("modal")
+let CLOSE = document.getElementsByClassName("close")[0];
+
+
 let STATES = [];
 fetch("./states.json")
     .then(function (response) {
@@ -17,20 +21,32 @@ for (let i = 0; i < states.length; i++) {
     states[i].addEventListener("click", (e) => {
         STATES.forEach((state) => {
             if (state.id == e.target.id) {
-                console.log(state);
                 createCart(state);
+                MODAL.style.display = "block"
             }
         });
-        console.log(e.target.id);
     });
 }
 
 
 function createCart(church) {
-    CART.innerHTML = `
+    CART.innerHTML = ""
+    CART.innerHTML += `
+        <span class="close" onclick="closeModal()">&times;</span>
         <p>${church.name}</p>
         <p>Location: ${church.location}</p>
-        <img src="${church.img}" alt="" class="family-picture">
+        <img src="${church.img}" alt="family" class="family-picture">
         <p>Family: ${church.family}</p>
     `;
 }
+
+function closeModal() {
+    MODAL.style.display = "none"
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  } 
