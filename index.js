53d9f4f58f97c0731af21b710073ccba2633c558
mike <josemimore0@gmail.com>
2024-05-13 12:30:14 -0400

    refactor the createChurch function

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const CART = document.getElementById("cart");
+const CARD = document.getElementById("card");
 const MODAL = document.getElementById("modal");
 let CLOSE = document.getElementsByClassName("close")[0];
 
@@ -18,7 +18,11 @@ fetch("./states.json")
 function clickPoint(e) {
     STATES.forEach((state) => {
         if (state.id == e.target.id) {
-            createCart(state);
+            if (!state.multiChurchState) {
+                createChurchCard(state);
+            } else {
+                createMultiChurchCard(state);
+            }
             MODAL.style.display = "block";
         }
     });
@@ -26,7 +30,7 @@ function clickPoint(e) {
 
 function printPoints(states) {
     let map = document.getElementById("map");
-    for (let i = 0; i < states.length; i++) {
+    for (let i = 0; i <= states.length; i++) {
         let point = document.createElement("div");
         point.className = "state";
         point.id = states[i].id;
@@ -35,22 +39,111 @@ function printPoints(states) {
     }
 }
 
-function createCart(church) {
-    CART.innerHTML = `
-        <span class="close" onclick="closeModal()">&times;</span>
-        <p class="churchName">${church.name}</p>
-        <div class="location"> 
-        <a href="${church.locationUrl ? church.locationUrl : "#"}" target="${
-        church.locationUrl ? "_blank" : ""
-    }">
-        <span class="locationIcon"></span>
-        </a>
-        <p> ${church.location}</p>
-        </div>
-        
-        <img src="${church.img}" alt="family" class="family-picture">
-        <p>Family: ${church.family}</p>
-    `;
+function createChurchCard(church, backId) {
+    CARD.innerHTML = "";
+    CARD.append(createCloseBtn(backId));
+    CARD.append(createTitle(church.name));
+    CARD.append(createLocationSection(church.location, church.locationUrl));
+    CARD.append(createFamilyImg(church.img));
+    CARD.append(createFamilyName(church.family));
+}
+
+function createMultiChurchCard(state) {
+    CARD.innerHTML = "";
+    CARD.append(createCloseBtn());
+    for (let i = 0; i <= state.churches.length - 1; i++) {
+        CARD.append(createChurchBox(state.churches[i], state.id));
+    }
+}
+
+function createChurchBox(church, backId) {
+    const churchBox = document.createElement("div");
+    churchBox.classList = "churchBox";
+
+    const churchText = document.createElement("p");
+    churchText.textContent = church.location;
+
+    churchBox.append(churchText);
+    churchBox.addEventListener("click", () => {
+        createChurchCard(church, backId);
+    });
+
+    return churchBox;
+}
+
+function createBackBtn(id) {
+    const back = document.createElement("div");
+    back.textContent = "◀";
+    back.classList = "backBtn";
+    back.addEventListener("click", () => {
+        console.log(id);
+        for (let i = 0; i <= STATES.length - 1; i++) {
+            if (STATES[i].id == id) {
+                createMultiChurchCard(STATES[i]);
+            }
+        }
+    });
+
+    return back;
+}
+
+function createCloseBtn(backId) {
+    btnsContainer = document.createElement("div");
+    if (backId) {
+        btnsContainer.append(createBackBtn(backId));
+    }
+    const span = document.createElement("span");
+    span.addEventListener("click", closeModal);
+    span.classList = "close";
+    span.innerHTML = "&times;";
+
+    btnsContainer.append(span);
+    btnsContainer.classList = "closeAndBackBtn";
+
+    return btnsContainer;
+}
+
+function createTitle(churchName) {
+    const p = document.createElement("p");
+    p.classList = "churchName";
+    p.textContent = churchName;
+    return p;
+}
+
+function createLocationSection(location, locationUrl) {
+    const locationSection = document.createElement("div");
+    locationSection.classList = "location";
+
+    const locationLink = document.createElement("a");
+    if (locationUrl) {
+        locationLink.setAttribute("href", locationUrl);
+        locationLink.setAttribute("target", "_black");
+    }
+
+    const mapIcon = document.createElement("span");
+    mapIcon.classList = "locationIcon";
+    locationLink.append(mapIcon);
+
+    const locationName = document.createElement("p");
+    locationName.textContent = location;
+
+    locationSection.append(locationLink);
+    locationSection.append(locationName);
+
+    return locationSection;
+}
+
+function createFamilyImg(imgURL) {
+    const img = document.createElement("img");
+    img.setAttribute("src", imgURL);
+    img.classList = "family-picture";
+    return img;
+}
+
+function createFamilyName(familyName) {
+    const p = document.createElement("p");
+    p.textContent = `Family: ${familyName}`;
+    return p;
 }
 
 function closeModal() {
@@ -59,7 +152,7 @@ function closeModal() {
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    if (event.target == MODAL) {
+        MODAL.style.display = "none";
     }
 };
